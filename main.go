645b4223bb5f6570a0c4de26a65599ee5ea50d42
zhy <8856450+jskcnsl@users.noreply.github.com>
2019-05-26 20:17:55 +0800

    feat: use sub command to handle proxy layer

--- main.go
+++ main.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-	"flag"
-
 	"github.com/jskcnsl/tower/command"
-	_ "github.com/jskcnsl/tower/help"
+	_ "github.com/jskcnsl/tower/proxy"
 )
 
 func main() {
-	flag.Parse()
-	cmd := flag.Args()
-	if len(cmd) == 0 {
-		if err := command.Get("help").Handle(); err != nil {
-			panic(err)
-		}
-	} else if c := command.Get(cmd[1]); c == nil {
-		if err := command.Get("help").Handle(); err != nil {
-			panic(err)
-		}
-	} else {
-		if err := c.Handle(); err != nil {
-			panic(err)
-		}
-	}
+	command.Execute()
 }
--- proxy/layer.go
+++ proxy/layer.go
@@ -1,5 +1,18 @@
 package proxy
 
+import (
+	"github.com/jskcnsl/tower/command"
+	"github.com/spf13/cobra"
+)
+
+var (
+	listenAddr  string
+	requestAddr string
+	socket      bool
+	tls         bool
+)
+
+// Layer define proxy layer struct
 type Layer struct {
 	ListenAddress  string
 	RequestAddress string
@@ -8,16 +21,42 @@ type Layer struct {
 	proxy          *Proxy
 }
 
+// Config set proxy layer
 func (l *Layer) Config(args ...interface{}) error {
-	l.ListenAddress = args[0].(string)
-	l.RequestAddress = args[1].(string)
-	l.SocketServer = args[2].(bool)
-	l.TLS = args[3].(bool)
+	l.ListenAddress = listenAddr
+	l.RequestAddress = requestAddr
+	l.SocketServer = socket
+	l.TLS = tls
 	return nil
 }
 
+// Run start proxy lyaer
 func (l *Layer) Run(args ...interface{}) error {
 	proxy := NewProxy(l.ListenAddress, l.RequestAddress, l.SocketServer, l.TLS)
 	l.proxy = proxy
+	l.proxy.Start()
 	return nil
+}
+
+func init() {
+	cmd := &cobra.Command{
+		Use:   "proxy",
+		Short: "Proxy layer start proxy server",
+		Long: `Proxy layer start proxy server to listen on one port,
+and transform request to the true handle server.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			l := new(Layer)
+			if err := l.Config(); err != nil {
+				panic(err)
+			}
+			if err := l.Run(); err != nil {
+				return
+			}
+		},
+	}
+	cmd.Flags().StringVar(&listenAddr, "listen", "0.0.0.0:8080", "proxy server listen on")
+	cmd.Flags().StringVar(&requestAddr, "handle", "", "server to handle request")
+	cmd.Flags().BoolVar(&socket, "socket", false, "whether handle request as socket")
+	cmd.Flags().BoolVar(&tls, "tls", false, "whether run proxy server with tls")
+	command.RegisterLayer(cmd)
 }
